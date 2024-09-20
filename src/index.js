b203fffbe3862bfe1c50557f1f138eb9566ba6e9
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-09-19 19:42:20 -0700

    Make selectProject
    allow the user to select what project they want to put the todo item in.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 
 class Todo {
-  constructor(title, description, dueDate) {
+  constructor(title, description, dueDate, project) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
+    this.project = project;
   }
 
   priority() {
@@ -29,7 +30,6 @@ class Todo {
 }
 
 class Project {
-  //stores todos
   constructor(name) {
     this.name = name;
   }
@@ -41,9 +41,17 @@ class Project {
   }
 }
 
-//show projects and some todos
+const projects = []
 
-//NEW TODO BUTTON
+//CREATE DEFAULT PROJECT
+const defaultProject = new Project('Default Project')
+projects.push(defaultProject)
+
+console.log(projects)
+
+//show projects and 3 todos on main content
+
+//ADD TODO BUTTON
 const addTodo = document.getElementById("addTodo");
 const todoDialog = document.getElementById("todoDialog");
 const title = todoDialog.querySelector("#title");
@@ -51,7 +59,20 @@ const description = todoDialog.querySelector("#description");
 const dueDate = todoDialog.querySelector("#dueDate");
 const confirmBtn = todoDialog.querySelector("#confirmBtn");
 
+const selectProject = todoDialog.querySelector("#selectProject");
+
 addTodo.addEventListener("click", () => {
+  for (let i = 0; i < projects.length; i++) {
+    const name = projects[i].name
+    const option = document.createElement("option")
+    option.textContent = name
+
+    option.value = projects[i].name
+    console.log(option.value)
+
+    selectProject.appendChild(option)
+  }
+
   todoDialog.showModal();
 });
 
@@ -61,11 +82,19 @@ todoDialog.addEventListener("close", (e) => {
 
 confirmBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  let newTodo = new Todo(title.value, description.value, dueDate.value)
+  let newTodo = new Todo(title.value, description.value, dueDate.value, selectProject.value)
+
+  //put todo in projects todo array
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].name === selectProject.value) {
+      projects[i].todos.push(newTodo)
+    }
+  }
+
   todoDialog.close();
 });
 
-//NEW PROJECT BUTTON
+//ADD PROJECT BUTTON
 const addProject = document.getElementById("addProject");
 const projectDialog = document.getElementById("projectDialog");
 const projectTitle = projectDialog.querySelector("#projectTitle");
@@ -82,5 +111,6 @@ projectDialog.addEventListener("close", (e) => {
 projectConfirmBtn.addEventListener("click", (event) => {
   event.preventDefault();
   let newProject = new Project(projectTitle.value)
+  projects.push(newProject)
   todoDialog.close();
 });
